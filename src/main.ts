9aacd45e08ef697228267d5c3010a62b30dbaadc
Sujoy Giri <girisujoy630@gmail.com>
2025-10-04 23:59:33 +0530

    revert samesite cookie to lax

--- src/main.ts
+++ src/main.ts
@@ -53,8 +53,8 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    // domain: process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'localhost',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    domain: process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'localhost',
+    sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'lax',
   }
 }));
 
